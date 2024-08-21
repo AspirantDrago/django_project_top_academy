9f159577d84fc663361e3fcf4e871fbf9a023688
Fattahov_d <damir802@yandex.ru>
2024-08-21 21:23:56 +0500

    прописать отношения в БД по заданию

--- pr_8/pr_8/urls.py
+++ pr_8/pr_8/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('products.urls', namespace='products')),
+    path('busket/', include('busket.urls', namespace='busket')),
+    path('salesman/', include('salesman.urls', namespace='salesman'))
 ]
